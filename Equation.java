698517630c0dc86adb83bc56e308717181f0d025
jackonkeyboard <jack14contact@gmail.com>
2023-05-11 22:04:31 +0400

    better api for Equations

--- Equation.java
+++ Equation.java
@@ -1,6 +1,48 @@
 import expression.*;
 
 public abstract class Equation {
+    protected class Solution {
+        public static enum Type {
+            ONE_SOLUTION, NO_SOLUTION, INFINITELY_SOLUTIONS, HOLDING, NOT_HOLDING
+        }
+
+        private Type type;
+        private double solution;
+
+        public Solution(Type type, double solution) {
+            this.type = type;
+            this.solution = solution;
+        }
+
+        public double getSolution() {
+            return solution;
+        }
+
+        public Type getType() {
+            return type;
+        }
+
+        @Override
+        public String toString() {
+            switch (type) {
+                case INFINITELY_SOLUTIONS:
+                    return "The equation has infinitely many solutions";
+                case NO_SOLUTION:
+                    return "The equation has no solutions";
+                case ONE_SOLUTION:
+                    return "Solution is: " + solution;
+                case HOLDING:
+                    return "The equation holds";
+                default:
+                    return "The equation does not hold";
+            }
+        }
+
+        public void print() {
+            System.out.println(toString());
+        }
+    }
+
     private Expression leftHandSide;
     private Expression rightHandSide;
 
@@ -18,5 +60,5 @@ public abstract class Equation {
         return rightHandSide;
     }
 
-    public abstract void displaySolution();
+    public abstract Solution solve();
 }
--- LinearEquation.java
+++ LinearEquation.java
@@ -5,40 +5,39 @@ public class LinearEquation extends Equation {
         super(eq);
     }
 
-    @Override
-    public void displaySolution() {
+    public Solution solve() {
         Expression left = getLeftHandSide();
         Expression rigt = getRightHandSide();
         Expression simpleForm = left.subtract(rigt);
 
-        if(simpleForm instanceof FirstOrderExpression){
+        if (simpleForm instanceof FirstOrderExpression) {
             double[] coefficients = simpleForm.getValue();
             double coefficient1 = coefficients[0];
             double coefficient2 = coefficients[1];
-            //almost zero
-            if(Math.abs(coefficient2) < 2 * Double.MIN_VALUE && Math.abs(coefficient1) < 2 * Double.MIN_VALUE){
-                System.out.println("The equation has infinitely many solutions");
-            }else if(Math.abs(coefficient2) < 2 * Double.MIN_VALUE && Math.abs(coefficient1) > 2 * Double.MIN_VALUE){
-                System.out.println("The equation has no solutions");
-            }else if(Math.abs(coefficient2) > 2 * Double.MIN_VALUE && Math.abs(coefficient1) < 2 * Double.MIN_VALUE){
-                System.out.println("Solution is: 0");
-            }else{
-                System.out.print("Solution is: ");
-                System.out.println(-(coefficient1/coefficient2));
-            }
 
-        }else if(simpleForm instanceof ConstantExpression){
+            // almost zero
+            if (Math.abs(coefficient2) < 2 * Double.MIN_VALUE && Math.abs(coefficient1) < 2 * Double.MIN_VALUE) {
+                return new Solution(Solution.Type.INFINITELY_SOLUTIONS, 0);
+            } else if (Math.abs(coefficient2) < 2 * Double.MIN_VALUE && Math.abs(coefficient1) > 2 * Double.MIN_VALUE) {
+                return new Solution(Solution.Type.NO_SOLUTION, 0);
+            } else if (Math.abs(coefficient2) > 2 * Double.MIN_VALUE && Math.abs(coefficient1) < 2 * Double.MIN_VALUE) {
+                return new Solution(Solution.Type.ONE_SOLUTION, 0);
+            } else {
+                return new Solution(Solution.Type.ONE_SOLUTION, -(coefficient1 / coefficient2));
+            }
+        } else { // case when simpleForm is an instanceof ConstantExpression
             double value = simpleForm.getValue()[0];
-            if(Math.abs(value) > 2 * Double.MIN_VALUE){
-                System.out.println("The equation does not hold");
-            }else{
-                System.out.println("The equation holds");
+            if (Math.abs(value) > 2 * Double.MIN_VALUE) {
+                return new Solution(Solution.Type.NOT_HOLDING, 1);
+            } else {
+                return new Solution(Solution.Type.HOLDING, 1);
             }
         }
     }
 
     public static void main(String[] args) {
         LinearEquation a = new LinearEquation(args[0]);
-        a.displaySolution();
+        Solution s = a.solve();
+        s.print();
     }
 }
